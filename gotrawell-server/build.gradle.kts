4fca5cfdd384d22813bcf5d86ec977c7d714570c
Filip Zolyniak <72789688+Zoltw@users.noreply.github.com>
2023-11-04 20:54:34 +0100

    fixes

--- gotrawell-server/build.gradle.kts
+++ gotrawell-server/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.xerial:sqlite-jdbc:3.43.2.2")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
